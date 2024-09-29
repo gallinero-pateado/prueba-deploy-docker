0ea12a2cf7386060cb68f097567eefe48d4d6978
garaneda <garaneda@utem.cl>
2024-09-29 20:37:13 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', async (req, res) => {
+	console.log("usuario ingreso con exito");
+	res.send("<h1>Todo OK<h1>")
+});
+
+app.listen(3000, () => console.log('servidor escuchando en el puero 3000...'))
